88aeb6372e36ce7df5a984a65c6f99660dd82932
sathishsuriya <sathishsuriya@gmail.com>
2021-11-15 13:57:30 +0530

    friend
    Signed-off-by: sathishsuriya <sathishsuriya@gmail.com>

---
+++ BasicProgram.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class BasicProgram {
+	
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+
+	int x=100;
+	switch(x)
+	{
+	case 10:
+		System.out.println("x is 10");
+		break;
+	case 20:
+		System.out.println("x is 20");
+		break;
+	case 100:
+		System.out.println("x is 100");
+		break;
+	case 120:
+		System.out.println("x is 120");
+		break;
+	}
+	
+}
+
+	}
+
+
+
+
+
+
+	
+
+
